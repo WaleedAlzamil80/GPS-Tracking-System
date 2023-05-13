f23ea92def9e936a4fe4cf580343b259bbd4ca2b
bisho10 <132770755+bisho10@users.noreply.github.com>
2023-05-13 23:29:00 +0300

    Add files via upload

---
+++ LCD_INS.c
@@ -0,0 +1,70 @@
+#include "main.h"
+
+void LCD4bits_Init(void)
+{
+	SYSCTL_RCGCGPIO_R |= 0x02;    //enable clock for PORTB
+	delayMs(10);                 //delay 10 ms for enable the clock of PORTB
+  GPIO_PORTB_DIR_R = 0xFF;             //let PORTB as output pins
+	GPIO_PORTB_DEN_R = 0xFF;             //enable PORTB digital IO pins
+	LCD4bits_Cmd(0x28);          //2 lines and 5x7 character (4-bit data, D4 to D7)
+	LCD4bits_Cmd(0x06);          //Automatic Increment cursor (shift cursor to right)
+	LCD4bits_Cmd(0x01);					 //Clear display screen
+	LCD4bits_Cmd(0x0F);          //Display on, cursor blinking
+}
+
+
+void LCD_Write4bits(unsigned char data, unsigned char control)
+{
+	data &= 0xF0;                       //clear lower nibble for control 
+	control &= 0x0F;                    //clear upper nibble for data
+	GPIO_PORTB_DATA_R = data | control;         //Include RS value (command or data ) with data 
+	GPIO_PORTB_DATA_R = data | control | EN;    //pulse EN
+	delayUs(0);													//delay for pulsing EN
+	GPIO_PORTB_DATA_R = data | control;					//Turn off the pulse EN
+	GPIO_PORTB_DATA_R = 0;                      //Clear the Data 
+}
+
+void LCD_WriteString(char * str)
+{  
+	volatile int i = 0;          //volatile is important 
+	
+	while(*(str+i) != '\0')       //until the end of the string
+	{
+		LCD4bits_Data(*(str+i));    //Write each character of string
+		i++;                        //increment for next character
+	}
+}
+
+void LCD4bits_Cmd(unsigned char command)
+{
+	LCD_Write4bits(command & 0xF0 , 0);    //upper nibble first
+	LCD_Write4bits(command << 4 , 0);			 //then lower nibble
+	
+	if(command < 4)
+		delayMs(2);       //commands 1 and 2 need up to 1.64ms
+	else
+		delayUs(40);      //all others 40 us
+}
+
+void LCD4bits_Data(unsigned char data)
+{
+	LCD_Write4bits(data & 0xF0 , RS);   //upper nibble first
+	LCD_Write4bits(data << 4 , RS);     //then lower nibble
+	delayUs(40);												//delay for LCD (MCU is faster than LCD)
+}
+
+void delayMs(int n)
+{  
+	volatile int i,j;             //volatile is important for variables incremented in code
+	for(i=0;i<n;i++)
+		for(j=0;j<3180;j++)         //delay for 1 msec
+		{}
+}
+
+void delayUs(int n)             
+{
+	volatile int i,j;							//volatile is important for variables incremented in code
+	for(i=0;i<n;i++)
+		for(j=0;j<3;j++)            //delay for 1 micro second
+		{}
+}
