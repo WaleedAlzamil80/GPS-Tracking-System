5dcf3323e0aa31ded8738d3591b5d0ea39d119b0
Maryyam3 <130384091+Maryyam3@users.noreply.github.com>
2023-05-13 23:00:13 +0300

    Add files via upload

---
+++ UART_Init.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void UART5_init (void){ 
+	SYSCTL_RCGCUART_R |= 0x0020;
+	while((SYSCTL_PRUART_R & 0x0020)==0){};
+	UART5_CTL_R &= ~0x0001;
+	UART5_IBRD_R = 104; 
+	UART5_FBRD_R = 11;
+	UART5_LCRH_R = 0x0070;
+	UART5_CTL_R = 0x0301;
+}
+
+char UART_InChar(void)
+	{
+	while ((UART5_FR_R & 0x10) != 0 ) {};
+		return ((char) (UART5_DR_R & 0xFF));
+	}
